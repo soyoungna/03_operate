ecd9e94a9cc99c1f3da4e9047aedb6e03542f9e3
soyoungna <hos05360@naver.com>
2018-10-02 16:01:19 +0900

    03_operate_3

--- 03_operate_1.c
+++ 03_operate_1.c
@@ -5,13 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	int sec;
+	int year;
+	int result;
 	
 	printf("input : ");
-	 scanf("%d", &sec);
+	 scanf("%d", &year);
 	 
+	 result = (year%4 == 0 && year%100 != 0) || (year%400 == 0);
 	 
-	 printf("%d sec. is %i : %i\n", sec, sec/60, sec%60);
+	 printf("leap year? %d\n", result);
 	 
 	 
 	return 0;
